cee33b3cb8a31ca81e9766786db58df1341f52ea
Henrique <henriquesc@gmail.com>
2017-09-01 17:43:56 -0400

    wow

---
+++ README.md
@@ -0,0 +1,14 @@
+# Files that start with dots and that are mine
+
+- Files in ./home should be dotfiles or directories
+  - Files/directories in the top level of this directory
+    will receive a "." in front of their names, this
+    means that dot files such as .bashrc and directories
+    such as .config should NOT have a dot in their names.
+    This only applies to files and directories in the top
+    level.
+  - Files in ./home will be moved to ~/
+- Files in ./fonts should be fonts, and will be moved
+  to /usr/share/fonts
+- The "bundle" directory in .vim and .vimpkg should
+  stay the same if you install more packages
---
+++ install.sh
@@ -0,0 +1,208 @@
+#!/bin/bash
+
+# Used to delete a file
+delete() {
+  if [ ! $# = 1 ]
+  then
+    echo "Usage for delete function: delete <file>"
+    exit 1
+  fi
+  if [ -e $1 ]
+  then
+    rm -rf "$1"
+    if $?
+    then
+      echo "  OK - Deleted file $1"
+    else
+      exit 1
+    fi
+  else
+    echo "  OK - File $1 does not exist - not deleting"
+  fi
+}
+
+
+# Copies a file or directory to a destination
+copy() {
+  if [ ! $# = 2 ]
+  then
+    echo "Usage for copy function: copy <file> <file>"
+    exit 1
+  fi
+  cp -r "$1" "$2"
+  if $?
+  then
+    echo "  OK - Copied $1 to $2"
+  else
+    exit 1
+  fi
+}
+
+# Installs the ./home folder
+installHome() {
+  for file in $(ls home)
+  do
+    oldFilePath="./home/$file"
+    newFilePath="$HOME/.$file"
+    delete "$newFilePath"
+    copy "$oldFilePath" "$newFilePath"
+  done
+}
+
+# Installs the fonts
+installFonts() {
+  cp -r "./fonts/*" "/usr/share/fonts"
+  if $?
+  then
+    echo "  OK - Fonts installed"
+  else
+    exit 1
+  fi
+}
+
+# Prompts the user for y or n - returns 0 for yes, 1 for n
+prompt() {
+  if [ ! $# = 1 ]
+  then
+    echo "Usage for prompt function: prompt <message> (message should not have '(Y/n)')"
+    exit 1
+  fi
+  read -rn1 -p "$1 (Y/n)"
+  if [[ $REPLY =~ [yY] ]]
+  then
+    return 0
+  else
+    return 1
+  fi
+}
+
+installApps() {
+  # AUR
+  if prompt "Install AUR?"
+  then
+    sudo pacman -S yaourt &&\
+    yaourt -Syy # Syncing
+  fi
+
+  # i3 and i3blocks
+  if prompt "Install i3 and i3blocks?"
+  then
+    sudo pacman -S i3 dmenu &&\
+    yaourt -S i3blocks
+  fi
+
+  # Essentials
+  if prompt "Install Essentials?"
+  then
+    sudo pacman -S base-devel wget file abs
+  fi
+
+  # Transmission
+  if prompt "Install Transmission?"
+  then
+    sudo pacman -S transmission-gtk
+  fi
+
+  # Sensors
+  if prompt "Install Sensors?"
+  then
+    sudo pacman -S lm_sensors
+  fi
+
+  # Java
+  if prompt "Install Java?"
+  then
+    sudo pacman -S java-runtime-common java-environment-common &&\
+    yaourt -S jdk-devel jre-devel
+  fi
+
+  # node
+  if prompt "Install node?"
+  then
+    sudo pacman -S npm &&\
+    sudo npm install -g n &&\
+    sudo n latest
+  fi
+
+  # Sublime
+  if prompt "Install Sublime?"
+  then
+    yaourt -S sublime-text
+  fi
+
+  # insync
+  if prompt "Install insync?"
+  then
+    yaourt -S insync
+  fi
+
+  # VLC
+  if prompt "Install VLC?"
+  then
+    yaourt -S vlc-git
+  fi
+
+  # PDF viewer
+  if prompt "Install PDF viewer?"
+  then
+    sudo pacman -S zathura
+  fi
+
+  # VIM
+  if prompt "Install VIM?"
+  then
+    sudo pacman -S vim
+  fi
+
+  # Nautilus
+  if prompt "Install Nautilus?"
+  then
+    sudo pacman -S nautilus
+  fi
+
+  # Scrot
+  if prompt "Install Scrot?"
+  then
+    sudo pacman -S scrot
+  fi
+
+  # Vokoscreen
+  if prompt "Install Vokoscreen?"
+  then
+    sudo pacman -S vokoscreen
+  fi
+
+  # Music
+  if prompt "Install Music?"
+  then
+    yaourt -S spotify
+  fi
+
+  # Docker
+  if prompt "Install Docker?"
+  then
+    sudo pacman -S docker
+  fi
+
+  # Network manager
+  if prompt "Install Network manager?"
+  then
+    sudo pacman -S networkmanager network-manager-applet
+  fi
+
+  # Haskell
+  if prompt "Install Haskell?"
+  then
+    sudo pacman -S ghc cabal-install happy alex
+  fi
+}
+
+# Installing apps
+echo "- Installing apps"
+installApps
+
+# Installing dot files and fonts
+echo "- Installing home dot files"
+installHome
+echo "- Installing fonts"
+installFonts
