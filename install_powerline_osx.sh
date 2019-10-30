128fe7a98a9afe7e1e0a0e98bae0009790575acc
Henry S. Coelho <henry@ritual.co>
2019-10-30 13:02:31 -0400

    wow

--- install_powerline_osx.sh
+++ install_powerline_osx.sh
@@ -6,6 +6,6 @@ powerline-daemon -q
 POWERLINE_BASH_CONTINUATION=1
 POWERLINE_BASH_SELECT=1
 . /usr/local/lib/python3.7/site-packages/powerline/bindings/bash/powerline.sh
-' >> ~/.bashrc.work
+' >> ~/.bash_profile
 
 echo 'If you are using iTerm2, setting Preferences > Text > Non-ASCII Font to "12pt Meslo LG S Regular for Powerline"'
